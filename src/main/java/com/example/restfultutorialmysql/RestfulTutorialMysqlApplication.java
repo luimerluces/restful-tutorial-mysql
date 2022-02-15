83d620467ed4e2f50dc1f40628aba2aa9092e091
bgdnvk <bnovykov@gmail.com>
2022-02-15 21:05:36 +0100

    Initial commit

---
+++ src/main/java/com/example/restfultutorialmysql/RestfulTutorialMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.restfultutorialmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulTutorialMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulTutorialMysqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restfultutorialmysql/RestfulTutorialMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restfultutorialmysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulTutorialMysqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
