37e3153a5de6277ba12599f572365e874edcfbc2
bgdnvk <bnovykov@gmail.com>
2022-02-17 23:50:08 +0100

    repository methods

--- src/main/java/com/example/restfultutorialmysql/repository/DataExampleRepository.java
+++ src/main/java/com/example/restfultutorialmysql/repository/DataExampleRepository.java
@@ -4,9 +4,20 @@ import com.example.restfultutorialmysql.entity.DataExample;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 //this is the implementation of the DAO interface
 //data access object which lets you access and modify data
 //the parameters are <TypeOfData, TypeOfId>
 @Repository
 public interface DataExampleRepository extends JpaRepository<DataExample, Long> {
+
+    //get data by email
+    List<DataExample> findDataExampleByEmail(String email);
+
+    //get data by name
+    List<DataExample> findDataExampleByName(String name);
+
+    //get data by id
+    List<DataExample> findDataExampleByDataId(Long id);
 }
