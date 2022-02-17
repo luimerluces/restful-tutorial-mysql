e77b12d43e7a3175bedc6d61d41293c3ab2fe896
bgdnvk <bnovykov@gmail.com>
2022-02-17 23:37:21 +0100

    repository

--- src/main/java/com/example/restfultutorialmysql/repository/DataExampleRepository.java
+++ src/main/java/com/example/restfultutorialmysql/repository/DataExampleRepository.java
@@ -1,10 +1,12 @@
 package com.example.restfultutorialmysql.repository;
 
+import com.example.restfultutorialmysql.entity.DataExample;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 //this is the implementation of the DAO interface
 //data access object which lets you access and modify data
 //the parameters are <TypeOfData, TypeOfId>
 @Repository
-public interface DataExampleRepository <DataExample, Long>{
+public interface DataExampleRepository extends JpaRepository<DataExample, Long> {
 }
