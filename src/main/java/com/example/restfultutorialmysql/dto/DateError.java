e856877ac3b9f5f704b40537cffda1d4db81e916
luimerluces <36189585+luimerluces@users.noreply.github.com>
2023-06-06 05:37:42 -0400

    06-06-2023

--- src/main/java/com/example/restfultutorialmysql/dto/DateError.java
+++ src/main/java/com/example/restfultutorialmysql/dto/DateError.java
@@ -3,20 +3,12 @@ package com.example.restfultutorialmysql.dto;
 import java.io.Serializable;
 
 public class DateError implements Serializable {
-
-    private static final long serialVersionUID = 1L;
-    
+    private static final long serialVersionUID = 1L;    
     private String date; 
-    
-
     public String getDate() {
         return date;
     }
-
     public void setDate(String fecha) {
         this.date = date;
     }
-            
-            
-    
 }
